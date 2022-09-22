020b7040707391a2bcbf5b6b04ecebb88fc7423f
Rafael Livise <r.livise@hotmail.com>
2022-09-21 21:20:08 -0500

    feat: add wallet conntect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import { ethers } from 'ethers';
+
 
 function App() {
+  const [walletAccount, setWalletAccount] = useState("");
+
+  const checkIfMetaMaskIsConnected = async () => {
+    try {
+      console.log("cargando ...")
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Check if Metamask is installed.");
+    } else {
+      console.log("you have metamask");
+    }
+
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+
+    if (accounts.length != 0) {
+      setWalletAccount(accounts[0]);
+    } else {
+      console.log("No authorized account");
+    }
+    } catch (error) {
+      console.log(error)
+    }
+  };
+
+  useEffect(() => {
+    checkIfMetaMaskIsConnected();
+  }, []);
+
+  const connectMetamask = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        alert("Get MetaMask");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      console.log(accounts[0]);
+      setWalletAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <button onClick={connectMetamask}>{walletAccount ?walletAccount :"conectate"}</button>
+        {/* {wallet} */}
         </p>
         <a
           className="App-link"
